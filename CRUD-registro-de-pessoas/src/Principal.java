21cef66d72e556927def4f2d04c603f52db1c338
carlosvinicios <carlosvinicios@id.uff.br>
2022-10-19 20:45:39 -0300

    Configuracoes da conexao ao banco de dados criadas!

--- CRUD-registro-de-pessoas/src/Principal.java
+++ CRUD-registro-de-pessoas/src/Principal.java
@@ -1,8 +1,7 @@
-
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- CRUD-registro-de-pessoas/src/repositorio/ClienteDAO.java
+++ CRUD-registro-de-pessoas/src/repositorio/ClienteDAO.java
@@ -1,5 +1,58 @@
 package repositorio;
 
-public class ClienteDAO {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 
+public class ClienteDAO {
+	
+	private Connection conexao;
+	private PreparedStatement pstmt;
+	private Statement stmt;
+	
+	public ClienteDAO() {
+		conexao = getConexao();
+	}
+	
+	private Connection getConexao() {
+			
+		try {
+			Properties prop = getProperties();
+			final String url = prop.getProperty("banco.url");
+			final String usuario = prop.getProperty("banco.usuario");
+			final String senha = prop.getProperty("banco.senha");
+			return DriverManager.getConnection(url, usuario, senha);
+		}
+			
+		catch(SQLException e) {
+			System.out.println("Erro nas configuracoes do Banco de Dados!");
+			return null;
+		}
+	}
+		
+	private Properties getProperties(){
+		
+		try {
+			Properties prop = new Properties();
+			String caminho = "/conexao.properties";
+			prop.load(ClienteDAO.class.getResourceAsStream(caminho));
+			return prop;
+		}
+		
+		catch(IOException e) {
+			System.out.println();
+			return null;
+		}
+	}
+	
+	//criar metodos
+	//Criar cliente()
+	//Consultar cliente()
+	//Alterar cliente()
+	//Remover Cliente()
+		
 }
