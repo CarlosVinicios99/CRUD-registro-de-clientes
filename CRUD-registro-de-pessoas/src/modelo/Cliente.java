f907c8c723aa8a4dc38098c13e67ffa128fafb42
carlosvinicios <carlosvinicios@id.uff.br>
2022-10-20 10:52:53 -0300

    Classe cliente implementada.

--- CRUD-registro-de-pessoas/src/modelo/Cliente.java
+++ CRUD-registro-de-pessoas/src/modelo/Cliente.java
@@ -1,5 +1,24 @@
 package modelo;
 
 public class Cliente {
-
+	
+	private String nome;
+	private int idade;
+	private String cpf;
+	private String cidade;
+	private String estado;
+	
+	public Cliente(String nome, int idade, String cpf, String cidade, String estado) {
+		this.nome = nome;
+		this.idade = idade;
+		this.cpf = cpf;
+		this.cidade = cidade;
+		this.cpf = cpf;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Nome: %s\nIdade: %d\nCPF: %s\nCidade: %s - %s",nome, idade, cpf, cidade, estado);
+	}
+	
 }
